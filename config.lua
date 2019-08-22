0cb5f2769e28e32405e6bc63459e7f84cd2d2697
esterfogaca <ester@cantarim.com>
2019-08-21 23:14:07 -0300

    feat(graphics): Added paddles and ball

--- config.lua
+++ config.lua
@@ -3,6 +3,8 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+FONT_SIZE = 48
+
 LOVE_CONFIG = {
     fullscreen = false,
     resizable = false,
--- main.lua
+++ main.lua
@@ -4,6 +4,9 @@ push = require "lib.push"
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    smallFont = love.graphics.newFont("assets/fonts/font.ttf", 48)
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(
         VIRTUAL_WIDTH,
         VIRTUAL_HEIGHT,
@@ -21,12 +24,18 @@ end
 function love.draw()
     push:apply('start')
 
+    love.graphics.clear(0.8, 0.4, 0.4, 1)
+
     love.graphics.printf(
         'Pawng!',
         0,
-        VIRTUAL_HEIGHT/2 - 6,
+        VIRTUAL_HEIGHT/2 - FONT_SIZE/2,
         VIRTUAL_WIDTH,
         'center')
-    
+
+    love.graphics.rectangle("fill", 10, 30, 5, 20)
+    love.graphics.rectangle("fill", VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.circle("fill", VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 3 - 60, 3)
+
     push:apply('end')
 end
