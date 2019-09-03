da371d857b13b74d57f6d7e86164eb841726271e
esterfogaca <ester@cantarim.com>
2019-09-03 00:16:47 -0300

    feat(collision): Added collision and score logic

--- _Ball.lua
+++ _Ball.lua
@@ -5,15 +5,25 @@ function Ball:init(x, y, size)
     self.y = y
     self.size = size
 
-    self.dy = math.random(2) == 1 and -100 or 100
-    self.dx = math.random(-50, 50)
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy = math.random(-50, 50)
+end
+
+function Ball:collides(paddle)
+    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.size then
+        return false
+    end
+    if self.y > paddle.y + paddle.height or paddle.y > self.y + self.size then
+        return false
+    end
+    return true
 end
 
 function Ball:reset()
     self.x = VIRTUAL_WIDTH / 2 - self.size / 2
     self.y = VIRTUAL_HEIGHT / 2 - self.size / 2
-    self.dy = math.random(2) == 1 and -100 or 100
-    self.dx = math.random(-50, 50)
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy = math.random(-50, 50)
 end
 
 function Ball:update(dt)
